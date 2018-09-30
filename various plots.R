1aac377ac2bfda30d7e6ad2f5c1ded4e48040285
tirthankarpatnaik <um18193@stu.ximb.ac.in>
2018-09-30 12:29:03 +0530

    plots

---
+++ various plots.R
@@ -0,0 +1,7 @@
+#plot
+women
+plot(women)
+plot(women,type='l')
+plot(women,type='p',pch=15,col='red')
+
+plot(x=women$weight,y=women$height,pch=15,xlab = 'weight',ylab = 'height',col='red',type='b')
